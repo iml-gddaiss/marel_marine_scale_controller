881d26532b509d95417f66b8e4e85d17211ea28c
jeromejguay <jerome.guay@protonmail.com>
2023-04-14 21:23:55 -0400

    fixing paths

--- marel_marine_scale_controller/__init__.py
+++ marel_marine_scale_controller/__init__.py
@@ -1,12 +1,13 @@
 import os, sys
+from pathlib import Path
 
 VERSION = "1.0.0"
 
-PROGRAM_DIRECTORY = os.path.dirname(__file__)
+PROGRAM_DIRECTORY = Path(__file__).parent
 
-LUA_SCRIPT_PATH = os.path.join(PROGRAM_DIRECTORY, "static/marel_app.lua")
-CONFIG_PATH = os.path.join(PROGRAM_DIRECTORY, "config/gui_config.json")
-LOGO_PATH = os.path.join(PROGRAM_DIRECTORY, "static/logo.ico")
+LUA_SCRIPT_PATH = str(PROGRAM_DIRECTORY.joinpath("static/marel_app.lua"))
+CONFIG_PATH = str(PROGRAM_DIRECTORY.joinpath("config/gui_config.json"))
+LOGO_PATH = str(PROGRAM_DIRECTORY.joinpath("static/logo.ico"))
 
 
 COMM_PORT = 52212
