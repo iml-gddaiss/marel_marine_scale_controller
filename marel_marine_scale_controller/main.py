63db136056a22610d8c39ce7e9a3458aabb74955
jeromejguay <jerome.guay@protonmail.com>
2023-05-15 10:52:46 -0400

    fixing bug and loggins

--- marel_marine_scale_controller/main.py
+++ marel_marine_scale_controller/main.py
@@ -1,3 +1,4 @@
+import logging
 from pathlib import Path
 from marel_marine_scale_controller import LUA_SCRIPT_PATH, CONFIG_PATH, LOGO_PATH
 from marel_marine_scale_controller.gui import GUI
@@ -10,5 +11,6 @@ ABS_CONFIG_PATH = str(PROGRAM_DIRECTORY.joinpath(CONFIG_PATH))
 ABS_LOGO_PATH = str(PROGRAM_DIRECTORY.joinpath(LOGO_PATH))
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     gui = GUI(lua_script_path=ABS_LUA_SCRIPT_PATH, config_path=ABS_CONFIG_PATH, logo_path=ABS_LOGO_PATH)
     gui.run()
